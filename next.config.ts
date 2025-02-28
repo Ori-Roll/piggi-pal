303bad94a242da418d1e02cd42a98f1d28ff657a
Ori Roll <oriroll@gmail.com>
2025-02-28 07:29:07 +0200

    small next type fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   experimental: {
+    //@ts-expect-error - This is an experimental feature in next.js (as of now: next-canary) and is not yet in the types
     nodeMiddleware: true,
   },
 };
