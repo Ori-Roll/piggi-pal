639b164ff3062fc1b14c69874ddbb77c7bb6573c
Ori Roll <oriroll@gmail.com>
2025-02-28 21:10:03 +0200

    added notification lib added in providers

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -2,19 +2,27 @@ import { ReactNode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { MantineProvider } from '@mantine/core';
 import { SessionProvider } from 'next-auth/react';
+import { Notifications } from '@mantine/notifications';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
 import queryClient from '@/config/queryClient';
 import theme from '@/config/mantine';
+import ErrorBoundary from '@/components/base/providers/ErrorBoundary';
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </QueryClientProvider>
-    </SessionProvider>
+    <ErrorBoundary>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <MantineProvider theme={theme}>
+            <Notifications />
+            {children}
+          </MantineProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </ErrorBoundary>
   );
 };
 
