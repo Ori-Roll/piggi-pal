8d1e3083d52bbe766dff74fb8df104b31c2cca19
Ori Roll <oriroll@gmail.com>
2025-03-04 05:31:24 +0200

    missing AddTask file with updates to fit prev commit

---
+++ src/components/AddTask/AddTask.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { Text, useMantineTheme } from '@mantine/core';
+import { IconAward } from '@tabler/icons-react';
+import ModalsWrapper from '@/components/Modals/ModalWrapper';
+import TaskForm from '@/components/TaskForm/TaskForm';
+import { ChildAccount } from '@prisma/client';
+import ActionButton from '@/components/base/ActionButton/ActionButton';
+
+type AddTaskProps = {
+  selectedChildAccount?: Partial<ChildAccount> | null;
+};
+
+const AddTask = (props: AddTaskProps) => {
+  const { selectedChildAccount } = props;
+
+  const [modalOpened, setModalOpened] = useState(false);
+
+  const theme = useMantineTheme();
+
+  const toggleModalOpened = () => {
+    setModalOpened((prev) => !prev);
+  };
+  const handleAddTaskSubmit = () => {
+    setModalOpened(false);
+  };
+
+  return (
+    <>
+      <ModalsWrapper
+        title="ADD BONUS TASK"
+        opened={modalOpened}
+        onClose={toggleModalOpened}
+      >
+        {selectedChildAccount && (
+          <TaskForm
+            selectedChildAccount={selectedChildAccount}
+            onSubmitCallback={handleAddTaskSubmit}
+          />
+        )}
+      </ModalsWrapper>
+      <ActionButton
+        onClick={toggleModalOpened}
+        colorAccent={theme.colors.orange[4]}
+        rightSection={<IconAward size="2rem" color={theme.colors.orange[4]} />}
+        disabled={!selectedChildAccount}
+      >
+        <Text c={theme.colors.dark[5]}>Add Bonus task</Text>
+      </ActionButton>
+    </>
+  );
+};
+
+export default AddTask;
